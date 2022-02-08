91be144f963f38a685c6b0e569f341427078d012
ZinchenkoYanislav <ZinchenkoYanislav@users.noreply.github.com>
2022-02-08 14:42:09 +0200

    promise v2

--- app.js
+++ app.js
@@ -4,18 +4,31 @@ let wrapper = document.querySelector(".wrapper");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  if (validate()) {
-    fetchPlaceHolder(parseInt(input.value));
-  }
+    if (validate()) {
+        wrapper.innerHTML = "";
+        fetchPlaceHolder(parseInt(input.value));
+    }
 });
 
-function fetchPlaceHolder(numberId) {
-  fetch("https://jsonplaceholder.typicode.com/posts/")
+function fetchPlaceHolder(id) {
+  fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
     .then((response) => response.json())
     .then((json) => {
-      let elementId = json.find((elem) => elem.id === numberId);
-      printComment(elementId.body);
+        if(id > 100 || id < 1){
+            throw new ReferenceError(`ID with number ${id} don't have `)
+        }
+        printPost(json.title, json.body)
       console.log(json);
+    })
+    .then(() => {
+      fetch(`https://jsonplaceholder.typicode.com/comments?postId=${id}`)
+        .then((res) => res.json())
+        .then((json) => {
+          json.forEach((item) => {
+            printComment(item.email, item.body);
+          });
+          console.log(json);
+        });
     })
     .catch((error) => {
       console.log(error);
@@ -23,24 +36,41 @@ function fetchPlaceHolder(numberId) {
     });
 }
 
-function printComment(element) {
-  wrapper.innerHTML = "";
+function printComment(email, comment) {
   wrapper.insertAdjacentHTML(
-    "afterbegin",
-    `<div class="card">
+     "beforeend",
+    `<h5>Comment</h5>
+    <div class="card">
         <div class="card-body">
-          <h5 class="card-title">Сomment</h5>
-          <p class="card-text">${element}</p>
+          <h5 class="card-title">${email}</h5>
+          <p class="card-text">${comment}</p>
         </div>
-      </div>`
+    </div>`
+  );
+}
+function printPost(title, post) {
+  wrapper.insertAdjacentHTML(
+    "beforeend",
+    `<h5>Post</h5>
+    <div class="card mb-2">
+            <div class="card-body">
+                <h5 class="card-title">${title}</h5>
+                <p class="card-text">${post}</p>
+            </div>
+         </div>`
   );
 }
 
 function validate() {
   let validate = false;
   let errorNumber = document.querySelector(".errorNumber");
+  let inputValue = input.value;
   console.log(errorNumber);
-  if (parseInt(input.value) < 1 || parseInt(input.value) > 100) {
+  if (
+    parseInt(inputValue) < 1 ||
+    parseInt(inputValue) > 100 ||
+    !inputValue.trim()
+  ) {
     errorNumber.classList.remove("hidden");
     validate = false;
   } else {
